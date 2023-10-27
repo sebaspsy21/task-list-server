98517f28bef08c8ddfad382c8811184c1723e8a2
sebastian motta <sebasmotta192002@gmail.com>
2023-10-27 17:42:10 -0500

    se agrego el servidor

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3060;
+
+
+const tasks = [
+  { id: 1, description: 'Hacer la compra', completed: false },
+  { id: 2, description: 'Estudiar para el examen', completed: true },
+  { id: 3, description: 'Hacer ejercicio', completed: false },
+];
+
+// Ruta para obtener la lista de tareas en formato JSON
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto 3020 `);
+});
