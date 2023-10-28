1f58905c06635e87905f668316a4ca6747e36219
sebastian motta <sebasmotta192002@gmail.com>
2023-10-27 19:53:37 -0500

    se agrego autenticacion

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 const app = express();
+app.use(express.json());
 const port = 3060;
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find((u) => u.username === username && u.password === password);
+  
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+  
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+    res.json({ token });
+  });
+
+  app.get('/ruta-protegida', (req, res) => {
+    const token = req.header('Authorization');
+  
+    if (!token) {
+      return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Token inválido' });
+      }
+  
+      // Si llegamos a este punto, el token es válido, y puedes realizar acciones en esta ruta.
+      res.json({ mensaje: 'Ruta protegida accedida con éxito' });
+    });
+  });
+  
+
 
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
@@ -37,20 +74,6 @@ function validateHTTPMethodsMiddleware(req, res, next) {
   }
   
   app.use('/list-edit', validateEditMiddleware, listEditRouter);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const tasks = [
   { id: 1, description: 'Hacer la compra', completed: false },
